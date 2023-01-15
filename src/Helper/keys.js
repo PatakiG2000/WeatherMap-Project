d2bff8abf07842450fd63a3a2dc81c2786fd4453
PatakiGergo <pataki821@gmail.com>
2023-01-15 19:45:14 +0100

    Commit
    Commit

--- src/Helper/keys.js
+++ src/Helper/keys.js
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
 const pexelkey = "563492ad6f91700001000001bd54e03127bd4163a34a752edb5ed488";
+=======
+const pexelkey = "563492ad6f91700001000001bd54e03127bd4163a34a752edb5ed488"
+>>>>>>> parent of 2f11af8 (Update)
 
-export default pexelkey;
+
+export default pexelkey
